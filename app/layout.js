7bc4d35432193cc8283d441d5f9995c91a0f3820
Cezar <cezar.vargas@gmail.com>
2026-07-27 19:58:16 -0300

    adicionar meta tags de PWA e favicon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,19 @@ import './globals.css';
 export const metadata = {
   title: 'Clube do Cinema',
   description: 'App do Clube do Cinema',
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png',
+  },
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'Clube do Cinema',
+  },
+  formatDetection: {
+    telephone: false,
+  },
 };
 
 export default function RootLayout({ children }) {
